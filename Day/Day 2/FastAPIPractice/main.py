28450e9a6e4ca5eaf88bfc70b189ffc3038647b7
Spidey-056 <2025cs_ashutoshasati_b@nie.ac.in>
2026-09-22 13:29:40 +0530

    Path Parameters with type hints

--- Day/Day 2/FastAPIPractice/main.py
+++ Day/Day 2/FastAPIPractice/main.py
@@ -5,10 +5,6 @@ app = FastAPI()
 def home():
     return {"Page": "Home"}
 
-print("Second Line")
-
-
-
 @app.get("/about")
 def about():
     return {"page":"About","author":"Ashutosh"}
@@ -21,7 +17,13 @@ def health():
 def create_something():
     return {"Message":"Created"}
 
+#Path Parameters
 @app.get("/students/{usn}")
 def get_result(usn):
     return {"Result ":"Distinction","usn":usn}
+
+#Path Parameters with Type Hint
+@app.get("/candidate/{rollno}")
+def get_result(rollno : int):
+    return {"Result ":"Distinction","rollno":rollno,"type":str(type(rollno))}
 
