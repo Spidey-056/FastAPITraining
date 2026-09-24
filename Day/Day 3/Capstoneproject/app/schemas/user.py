1c38973e434f4bb3c9d613145b4387909fb0a299
Spidey-056 <2025cs_ashutoshasati_b@nie.ac.in>
2026-09-24 12:51:38 +0530

    Ticket files modified

--- Day/Day 3/Capstoneproject/app/schemas/user.py
+++ Day/Day 3/Capstoneproject/app/schemas/user.py
@@ -6,7 +6,7 @@ from typing import Optional
 
 from pydantic import BaseModel, EmailStr, Field
 
-from app.models.users import UserRole
+from app.models.user import UserRole
 
 class UserCreate(BaseModel):
     """Data required from the client when creating a new user (POST /users)."""
