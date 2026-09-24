8ced534f9bb9ab6eacab4142df34e5f16fe32637
Spidey-056 <2025cs_ashutoshasati_b@nie.ac.in>
2026-09-24 11:41:04 +0530

    Added ticket files and modified file name in models

--- Day/Day 3/Capstoneproject/app/main.py
+++ Day/Day 3/Capstoneproject/app/main.py
@@ -6,12 +6,14 @@ from app.config import settings
 from app.database import ping_database
 from app.routers import users
 from app.routers import categories
+from app.routers import tickets
 
 # Creating FastAPI app instance 
 app = FastAPI(title=settings.APP_NAME)
 
 app.include_router(users.router)
 app.include_router(categories.router)
+app.include_router(tickets.router)
 
 # This function runs once when the server starts.
 @app.on_event("startup")
