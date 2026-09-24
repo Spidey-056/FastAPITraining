20873d60fa75fa68981043787cc355343c78f421
Spidey-056 <2025cs_ashutoshasati_b@nie.ac.in>
2026-09-24 09:35:40 +0530

    Capstone Project

---
+++ Day/Day 3/Capstoneproject/app/config.py
@@ -0,0 +1,20 @@
+# This file defines all configuration values the app needs
+# (DB connection info, app name, etc)
+
+# pydantic_settings : Automatically reads environment variables and their types
+from pydantic_settings import BaseSettings,SettingsConfigDict
+
+class Settings(BaseSettings):
+
+    #MongoDB settings
+    MONGO_URI : str = "mongodb://localhost:27017"
+    MONGO_DB_NAME: str = "it_servicedesk"
+
+    #Gives the app a name
+    APP_NAME : str = "IT Service Desk App API"
+
+    #Informs pydantic_settings to load values from .env file for all the data
+    model_config = SettingsConfigDict(env_file=".env",env_file_encoding="utf-8")
+
+# Shared Settings Object that all other files can import 
+settings = Settings()
---
+++ Day/Day 3/Capstoneproject/app/main.py
@@ -0,0 +1,24 @@
+# This file is the entry point of the application
+
+from fastapi import FastAPI
+
+from app.config import settings
+from app.database import ping_database
+from app.routers import users
+
+# Creating FastAPI app instance 
+app = FastAPI(title=settings.APP_NAME)
+
+app.include_router(users.router)
+
+# This function runs once when the server starts.
+@app.on_event("startup")
+def on_startup() -> None:
+    if not ping_database():
+        raise RuntimeError("Could not connect to MongoDB")
+    print(f"[startup]Connected to MongoDB App:{settings.APP_NAME}")
+
+# Checks basic health check API endpoint and confirms GET / is running and reachable. (/ is condidered as 'root')
+@app.get("/",tags=["Health"])
+def health_check():
+    return {"status":"ok","app":settings.APP_NAME}
