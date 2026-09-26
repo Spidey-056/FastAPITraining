8e0f8d549b25a929b63e11272d699d63f4b7277e
2025csdeepakguptab-max <2025cs_deepakgupta_b@nie.ac.in>
2026-09-26 10:49:40 +0530

    feat: add seed data script to populate MongoDB with sample e-commerce records

--- Day/Day 1/P2.py
+++
@@ -1,2 +0,0 @@
-print("This is the second file")
-print("I edited this file")
--- Day/Day 3/Capstoneproject/app/__init__.py
+++
@@ -1,3 +0,0 @@
-# This file we will keep it empty.
-# This file tells python to treat the "app" folder as a package. 
-# So all other files can import it.
